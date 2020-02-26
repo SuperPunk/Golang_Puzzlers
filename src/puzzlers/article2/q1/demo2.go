0c422c649fa1f952411aabf0814b1ab5ecce250b
pakuwa <shylunule@gmail.com>
2020-02-26 14:55:46 +0800

    the init method

--- src/puzzlers/article2/q1/demo2.go
+++ src/puzzlers/article2/q1/demo2.go
@@ -8,9 +8,21 @@ import (
 var name string
 
 func init() {
+	fmt.Println("the first init method")
+}
+
+func init() {
+	fmt.Println("the second init method")
+}
+
+func init() {
+	fmt.Println("the init method for reading flag")
 	flag.StringVar(&name, "name", "everyone", "The greeting object.")
 }
 
+/*
+	init函数先于main函数执行。每个包可以有多个init函数，每个init函数执行顺序没确定。
+*/
 func main() {
 	flag.Parse()
 	fmt.Printf("Hello, %s!\n", name)
